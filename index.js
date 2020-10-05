67a611561d5ddadb48521bd2bc663678d8406ef0
jimmy404 <bruno_pier@hotmail.com>
2020-10-05 18:34:42 -0300

    Adds code to add user paths

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ conectarDB();
 //puerto de la app
 const PORT = process.env.PORT || 4000;
 
+//importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+
 //arranca app
 app.listen(PORT, () => {
   console.log(`El servidor esta funcionando en el puerto ${PORT}`)
---
+++ routes/usuarios.js
@@ -0,0 +1,11 @@
+//rutas para crear usuarios
+const express = require('express');
+const router = express.Router();
+
+//crea un usuario
+// api/usuarios
+router.post('/', () => {
+
+});
+
+module.exports = router;
