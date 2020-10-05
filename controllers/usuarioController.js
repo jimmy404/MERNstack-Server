c662d9823d74da3cb1d97faacb8f82211524dba7
jimmy404 <bruno_pier@hotmail.com>
2020-10-05 20:34:19 -0300

    Adds code to check if the user has not been previously registered

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,14 +1,23 @@
 const Usuario = require('../models/Usuario');
 
 exports.crearUsuario = async (req, res) => {
+
+  //extraer email y pass
+  const { email, password } = req.body;
+
+
   try {
-    let usuario;
+    //revisar que el usuario sea unico
+    let usuario = await Usuario.findOne({email});
+    if(usuario) {
+      return res.status(400).json({msg: 'El usuario ya existe'});
+    }
     //crea nuevo usuario
     usuario = new Usuario(req.body);
     //guardar usuario
     await usuario.save();
     //mensaje de confirmacion
-    res.send('Usuario creado correctamente');
+    res.json({msg: 'Usuario creado OK'})
   } catch (error) {
     console.log(error);
     res.status(400).send('Hubo un error');
