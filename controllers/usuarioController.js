508fd8a25f42abbe430b14dd6e6e48b4bac0e126
jimmy404 <bruno_pier@hotmail.com>
2020-10-05 20:38:46 -0300

    Code for Hash passwords in the database

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,5 @@
 const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
 
 exports.crearUsuario = async (req, res) => {
 
@@ -14,6 +15,9 @@ exports.crearUsuario = async (req, res) => {
     }
     //crea nuevo usuario
     usuario = new Usuario(req.body);
+    //hashear password
+    const salt = await bcryptjs.genSalt(10);
+    usuario.password = await bcryptjs.hash(password, salt);
     //guardar usuario
     await usuario.save();
     //mensaje de confirmacion
