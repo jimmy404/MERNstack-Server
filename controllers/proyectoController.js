cf880cd56df61ce9d33b5960e6e6e47c747695f7
jimmy404 <bruno_pier@hotmail.com>
2020-10-06 12:40:05 -0300

    Adds code for check if there is a project - who created it - edition

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -49,12 +49,19 @@ exports.actualizarProyecto = async(req, res) => {
 
     try {
       //revisar el ID
-
+      let proyecto = await Proyecto.findById(req.params.id);
       //revisar si existe proyecto
-
+      if(!proyecto){
+        return res.status(404).json({msg: 'Proyecto no encontrado'});
+      }
       //verificar el creador del proyecto
-
+      if(proyecto.creador.toString() !== req.usuario.id) {
+        return res.status(401).json({msg: 'No autorizado'});
+      }
       //actualizar
+      proyecto = await Proyecto.findByIdAndUpdate({ _id: req.params.id }, { $set: nuevoProyecto }, { new: true })
+      res.json({ proyecto });
+
     } catch (error) {
       console.log(error);
       res.status(500).send('Error en el servidor');
